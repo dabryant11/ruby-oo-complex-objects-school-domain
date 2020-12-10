e5cdf659c8187752bb74abf4b487fc58cb41c345
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2020-12-10 11:58:53 -0800

    commit tree 3d34ee807c07032e54abbabc42c835f292e0374c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_reader :name 
+    attr_accessor :roster
+
+    def initialize(name_par, roster_par = {})
+        @name = name_par
+        @roster = roster_par
+    end 
+  
+    def add_student(name_para, grade_para)
+
+        roster[grade_para] = name_para 
+        
+
+    end
+
+
+
+end 
+
+
+ruby  = School.new("Bayside High School")
+hs2 = School.new("High high school")
+binding.pry
+0
